8312ff050ef9020a925fc51ac37a96fdc1a1da24
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-01-18 09:35:14 +0200

    Implemented rendering for the first row of shapes

--- components/Games/Game4.tsx
+++ components/Games/Game4.tsx
@@ -1,202 +1,228 @@
 import "@babylonjs/inspector";
-import {AbstractMesh, ArcRotateCamera, Camera, Color3, Color4, Vector2, Vector3,} from '@babylonjs/core';
-import React, {ComponentProps, useState} from 'react';
-import {Engine, Scene, SceneEventArgs, useScene} from 'react-babylonjs';
-import {isMobile} from 'react-device-detect';
-import {createAxis} from '@components/Axis/axisHelper';
-import {Container} from '@components/common/Container';
-import {AXIS_SIZE, generateGUI} from "../../utils/GenerateGUI";
-import {SHAPE_NAME, SHAPE_SIZE} from "../../utils/GenerateFigure";
-import s from "../../styles/Proto.App.module.scss"
+import {
+  ArcRotateCamera,
+  Camera,
+  Color3,
+  Color4,
+  Mesh,
+  MeshBuilder,
+  Scalar,
+  StandardMaterial,
+  TransformNode,
+  Vector2,
+  Vector3,
+  Viewport,
+} from "@babylonjs/core";
+import React from "react";
+import { Box, Engine, Scene, SceneEventArgs, useScene } from "react-babylonjs";
+import { createAxis } from "@components/Axis/axisHelper";
+import { AXIS_SIZE, generateGUI } from "../../utils/GenerateGUI";
+import {
+  defaultConfig,
+  generateFigure,
+  generateRotation,
+  GenerationConfig,
+  SHAPE_NAME,
+  SHAPE_SIZE,
+} from "../../utils/GenerateFigure";
+import s from "../../styles/Proto.App.module.scss";
 import classNames from "classnames";
 
-type CameraConfig = Partial<ArcRotateCamera>
+type CameraConfig = Partial<ArcRotateCamera> & Partial<Viewport>;
 
 const defaultCameraConfig: CameraConfig = {
-    name: "camera-r",
-    alpha: Math.PI * .25,
-    beta: Math.PI * .3,
-    radius: 15,
-    target:new Vector3(0,0,0),
-}
-
-const camerasConfig: CameraConfig[] = [{
-
-}]
-
-const Cameras = () => {
-    const scene = useScene();
-    const alwaysActive = (camera: Camera) => {
-        if (scene) scene.activeCameras = [...(scene.activeCameras || []), camera];
-        console.log(scene);
-    };
-
-    return (
-        <>
-            <arcRotateCamera
-                name='camera1'
-                alpha={Math.PI * .25}
-                beta={Math.PI * .3}
-                radius={15}
-                target={new Vector3(1, 0, 0)}
-                onCreated={alwaysActive}
-            >
-                <viewport x={0.2} y={.66} height={.33} width={.2}/>
-            </arcRotateCamera>
-
-            <arcRotateCamera
-                name='camera2'
-                alpha={Math.PI * .25}
-                beta={Math.PI * .3}
-                radius={15}
-                target={new Vector3(3, 0, 0)}
-                cameraRotation={new Vector2(10, 15)}
-                onCreated={alwaysActive}
-            >
-                <viewport x={0.6} y={.66} height={.33} width={.2}/>
-            </arcRotateCamera>
-            <arcRotateCamera
-                name='camera3'
-                alpha={Math.PI * .25}
-                beta={Math.PI * .3}
-                radius={15}
-                target={new Vector3(2, 1, 0)}
-                cameraRotation={new Vector2(10, 15)}
-                onCreated={alwaysActive}
-            >
-                <viewport x={0.4} y={.33} height={.33} width={.2}/>
-            </arcRotateCamera>
-            <arcRotateCamera
-                name='camera4'
-                alpha={Math.PI * .25}
-                beta={Math.PI * .3}
-                radius={15}
-                target={new Vector3(0, 2, 0)}
-                cameraRotation={new Vector2(10, 15)}
-                onCreated={alwaysActive}
-            >
-                <viewport x={0} y={0} height={.33} width={.2}/>
-            </arcRotateCamera>
-            <arcRotateCamera
-                name='camera5'
-                alpha={Math.PI * .25}
-                beta={Math.PI * .3}
-                radius={15}
-                target={new Vector3(1, 2, 0)}
-                cameraRotation={new Vector2(10, 15)}
-                onCreated={alwaysActive}
-            >
-                <viewport x={0.2} y={0} height={.33} width={.2}/>
-            </arcRotateCamera>
-            <arcRotateCamera
-                name='camera6'
-                alpha={Math.PI * .25}
-                beta={Math.PI * .3}
-                radius={15}
-                target={new Vector3(2, 2, 0)}
-                cameraRotation={new Vector2(10, 15)}
-                onCreated={alwaysActive}
-            >
-                <viewport x={0.4} y={0} height={.33} width={.2}/>
-            </arcRotateCamera>
-            <arcRotateCamera
-                name='camera7'
-                alpha={Math.PI * .25}
-                beta={Math.PI * .3}
-                radius={15}
-                target={new Vector3(3, 2, 0)}
-                cameraRotation={new Vector2(10, 15)}
-                onCreated={alwaysActive}
-            >
-                <viewport x={0.6} y={0} height={.33} width={.2}/>
-            </arcRotateCamera>
-            <arcRotateCamera
-                name='camera8'
-                alpha={Math.PI * .25}
-                beta={Math.PI * .3}
-                radius={15}
-                target={new Vector3(4, 2, 0)}
-                cameraRotation={new Vector2(10, 15)}
-                onCreated={alwaysActive}
-            >
-                <viewport x={0.8} y={0} height={.33} width={.2}/>
-            </arcRotateCamera>
-        </>
-    );
+  name: "camera-r",
+  alpha: Math.PI * 0.25,
+  beta: Math.PI * 0.3,
+  radius: 15,
+  target: new Vector3(0, 0, 0),
+  height: 0.33,
+  width: 0.2,
 };
 
-const Game4 = () => {
-    const onSceneMount = (sceneEventArgs: SceneEventArgs) => {
-        const {scene} = sceneEventArgs;
-        const gui = generateGUI(sceneEventArgs);
-        scene.onDisposeObservable.add(() => gui.destroy());
-        createAxis(sceneEventArgs, AXIS_SIZE);
-        // scene.debugLayer.show();
+const camerasConfig: CameraConfig[] = [
+  { x: 0.2, y: 0.66, height: 0.33, width: 0.2 },
+  { x: 0.6, y: 0.66, height: 0.33, width: 0.2 },
+  { x: 0.4, y: 0.33, height: 0.33, width: 0.2 },
+  { x: 0, y: 0, height: 0.33, width: 0.2 },
+  {
+    x: 0.4,
+    y: 0,
+    height: 0.33,
+    width: 0.2,
+  },
+  {
+    x: 0.6,
+    y: 0,
+    height: 0.33,
+    width: 0.2,
+  },
+  {
+    x: 0.8,
+    y: 0,
+    height: 0.33,
+    width: 0.2,
+  },
+  {
+    x: 0.2,
+    y: 0,
+    height: 0.33,
+    width: 0.2,
+  },
+];
+
+export const POSITION_MULTIPLIER = 300;
+
+function createScene(sceneEventArgs: SceneEventArgs) {
+  const { scene, canvas } = sceneEventArgs;
+
+  const cameras = camerasConfig.map((configRaw, i) => {
+    const config = { ...configRaw, ...defaultCameraConfig };
+    const camera = new ArcRotateCamera(
+      `camera-${i}`,
+      config.alpha!,
+      config.beta!,
+      config.radius!,
+      new Vector3(
+        config.x! * POSITION_MULTIPLIER,
+        config.y! * POSITION_MULTIPLIER,
+        0
+      )!,
+      scene,
+      true
+    );
+    camera.viewport = new Viewport(
+      config.x!,
+      config.y!,
+      config.width!,
+      config.height!
+    );
+    // TODO: remove later
+    camera.attachControl(canvas, true);
+    scene.activeCameras = (scene.activeCameras || []).concat(camera);
+
+    return camera;
+  });
+
+  function getBoxName(i: number) {
+    return `box-${i}`;
+  }
+
+  const shapes = camerasConfig.map((configRaw, i) => {
+    const config = { ...configRaw, ...defaultCameraConfig };
+    const box = MeshBuilder.CreateBox(getBoxName(i), {
+      size: SHAPE_SIZE,
+    });
+    box.scaling = new Vector3(1, 1, 1);
+    box.position = new Vector3(
+      config.x! * POSITION_MULTIPLIER,
+      config.y! * POSITION_MULTIPLIER,
+      0
+    );
+    box.isVisible = false;
+    box.enableEdgesRendering();
+    box.edgesWidth = 5;
+    box.edgesColor = Color4.FromColor3(Color3.Black(), 1);
+
+    const material = new StandardMaterial(`box-material-${1}`, scene);
+    material.diffuseColor = new Color3(0.82, 0.82, 0.82);
+    material.specularColor = Color3.White();
+
+    box.material = material;
+    return box;
+  });
+
+  function getRandomAngle() {
+    const rotationTimes = Math.floor(Scalar.RandomRange(0, 4));
+    return rotationTimes * 90;
+  }
+
+  const rotateReferenceShape = () => {
+    const baseFigureConfig: GenerationConfig = {
+      ...defaultConfig,
+      originX: shapes[0].position.x,
+      originY: shapes[0].position.y,
     };
 
-    return (
-        <>
-            <div className={s.blockGrid}>
-                <div className={classNames(s.block1, s.block)}>is rotated to</div>
-                <div className={classNames(s.block2, s.block)}>as</div>
-                <div className={classNames(s.block3, s.block)}>is rotated to</div>
-                <div className={classNames(s.blockWithVariants, s.block)}>
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
-                    <div>5</div>
-                </div>
-            </div>
-            <Engine
-                antialias
-                adaptToDeviceRatio
-                canvasId='babylonJS'
-                canvasStyle={{
-                    flex: '1',
-                    display: 'flex',
-                    width: '100vw',
-                    height: '100vh',
-                    "padding-bottom": "4rem"
-                }}
-                debug
-            >
-                <Scene
-                    key='scene3'
-                    onSceneMount={onSceneMount}
-                    clearColor={Color4.FromColor3(Color3.White())}
-                    onKeyboardObservable={(...args: any[]) => console.log(args)}
-                    onPointerDown={(...args) => console.log(args)}
-                    onScenePointerDown={(...args) => console.log(args)}
-                >
-                    <Cameras/>
-                    <hemisphericLight
-                        name='light1'
-                        intensity={1}
-                        direction={Vector3.Up()}
-                        // direction={new Vector3(0, 0, 0)}
-                        groundColor={Color3.White()}
-                        specular={Color3.Black()}
-                    />
-                    <box
-                        isVisible={false}
-                        name={SHAPE_NAME}
-                        size={SHAPE_SIZE}
-                        position={new Vector3(0, 3, 0)}
-                        scaling={new Vector3(1, 1, 1)}
-                        enableEdgesRendering
-                        edgesWidth={5}
-                        edgesColor={Color4.FromColor3(Color3.Black(), 1)}
-                    >
-                        <standardMaterial
-                            name={`random-box-mat`}
-                            diffuseColor={new Color3(.82, .82, .82)}
-                            // specularColor={Color3.White()}
-                        />
-                    </box>
-                </Scene>
-            </Engine></>
-    );
+    generateFigure(sceneEventArgs, baseFigureConfig, shapes[0].name);
+
+    const { position } = shapes[1];
+    shapes[1].dispose();
+    shapes[1] = shapes[0].clone(getBoxName(1));
+    console.log({ position });
+
+    const transformParent = new TransformNode(`transform-${getBoxName(1)}`);
+    shapes[1].parent = transformParent;
+    shapes[1].position = position;
+
+    const rotation = generateRotation({
+      finalRotationX: getRandomAngle(),
+      finalRotationY: getRandomAngle(),
+      finalRotationZ: getRandomAngle(),
+    });
+    shapes[1].rotation = rotation;
+  };
+
+  rotateReferenceShape();
+}
+
+const Game4 = () => {
+  const onSceneMount = (sceneEventArgs: SceneEventArgs) => {
+    const { scene } = sceneEventArgs;
+    // const gui = generateGUI(sceneEventArgs);
+    // scene.onDisposeObservable.add(() => gui.destroy());
+    // createAxis(sceneEventArgs, AXIS_SIZE);
+    createScene(sceneEventArgs);
+    scene.debugLayer.show();
+  };
+
+  return (
+    <>
+      <div className={s.blockGrid}>
+        <div className={classNames(s.block1, s.block)}>is rotated to</div>
+        <div className={classNames(s.block2, s.block)}>as</div>
+        <div className={classNames(s.block3, s.block)}>is rotated to</div>
+        <div className={classNames(s.blockWithVariants, s.block)}>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+        </div>
+      </div>
+      <Engine
+        antialias
+        adaptToDeviceRatio
+        canvasId="babylonJS"
+        canvasStyle={{
+          flex: "1",
+          display: "flex",
+          width: "100%",
+          height: "100vh",
+          paddingBottom: "4rem",
+        }}
+        debug
+      >
+        <Scene
+          key="scene3"
+          onSceneMount={onSceneMount}
+          clearColor={Color4.FromColor3(Color3.White())}
+          onKeyboardObservable={(...args: any[]) => console.log(args)}
+          onPointerDown={(...args) => console.log(args)}
+          onScenePointerDown={(...args) => console.log(args)}
+        >
+          <hemisphericLight
+            name="light1"
+            intensity={1}
+            direction={Vector3.Up()}
+            // direction={new Vector3(0, 0, 0)}
+            groundColor={Color3.White()}
+            specular={Color3.Black()}
+          />
+        </Scene>
+      </Engine>
+    </>
+  );
 };
 
 export default Game4;
--- utils/GenerateFigure.tsx
+++ utils/GenerateFigure.tsx
@@ -1,141 +1,155 @@
-import {Mesh, Scalar, TransformNode, Vector3} from '@babylonjs/core'
-import {SceneEventArgs} from 'react-babylonjs'
-import {getRandomInt} from './common'
+import { Mesh, Scalar, TransformNode, Vector3 } from "@babylonjs/core";
+import { SceneEventArgs } from "react-babylonjs";
+import { getRandomInt } from "./common";
 
-export const SHAPE_NAME = 'box-figure'
-export const SHAPE_SIZE = 2
-const SHAPE_INITIAL_COORD = new Vector3(0, -2, 0)
+export const SHAPE_NAME = "box-figure";
+export const SHAPE_SIZE = 2;
+const SHAPE_INITIAL_COORD = new Vector3(0, -2, 0);
 
-const SHAPE_PARENT_NAME = 'figure_parent'
+const SHAPE_PARENT_NAME = "figure_parent";
 
 export type GenerationConfig = {
-  spreadOnX: number
-  spreadOnY: number
-  spreadOnZ: number
-  totalBlocks: number
-  maxDeltaForNextBlock: number
-  finalRotationX: number
-  finalRotationY: number
-  finalRotationZ: number
-  originX: number,
-  originY: number,
-  originZ: number,
-  showAxis: boolean,
-}
+  spreadOnX: number;
+  spreadOnY: number;
+  spreadOnZ: number;
+  totalBlocks: number;
+  maxDeltaForNextBlock: number;
+  finalRotationX: number;
+  finalRotationY: number;
+  finalRotationZ: number;
+  originX: number;
+  originY: number;
+  originZ: number;
+  showAxis: boolean;
+};
 
+export const defaultConfig: GenerationConfig = {
+  spreadOnX: 0.5,
+  spreadOnY: 0.5,
+  spreadOnZ: 0.5,
+  totalBlocks: 8,
+  maxDeltaForNextBlock: 1,
+  finalRotationX: 0,
+  finalRotationY: 0,
+  finalRotationZ: 0,
+  originX: 0,
+  originY: 0,
+  originZ: 0,
+  showAxis: true,
+};
 export const generateFigure = (
   sceneEventArgs: SceneEventArgs,
   config: GenerationConfig,
   shapeName = SHAPE_NAME
 ) => {
-  const { scene } = sceneEventArgs
-  const square = scene.getMeshByName(shapeName) as Mesh
-  const newCoords = generateCoordinates(config)
-  const rotation = generateRotation(config)
+  const { scene } = sceneEventArgs;
+  const square = scene.getMeshByName(shapeName) as Mesh;
+  const newCoords = generateCoordinates(config);
 
+  const rotation = generateRotation(config);
   newCoords.forEach((coord, i) => {
-    const inst = square.createInstance(`${shapeName}-${i}`)
-    inst.setParent(square)
-    inst.scalingDeterminant = 0.99
-    inst.position = coord
-  })
+    const inst = square.createInstance(`${shapeName}-${i}`);
+    inst.setParent(square);
+    inst.scalingDeterminant = 0.99;
+    inst.position = coord;
+  });
+  square.edgesShareWithInstances = true;
+  square.rotation = rotation;
 
-  square.edgesShareWithInstances = true
-  square.rotation = rotation
   square.position = new Vector3(config.originX, config.originY, config.originZ);
-
   const parent =
     scene.getTransformNodeByName(SHAPE_PARENT_NAME) ||
-    new TransformNode(SHAPE_PARENT_NAME)
-  square.setParent(parent)
+    new TransformNode(SHAPE_PARENT_NAME);
+  square.setParent(parent);
   // parent.position.x = -6
-}
-
-export const clearFigure = (sceneEventArgs: SceneEventArgs, shapeName = SHAPE_NAME) => {
-  const { scene } = sceneEventArgs
-  const square = scene.getMeshByName(shapeName) as Mesh
+};
+export const clearFigure = (
+  sceneEventArgs: SceneEventArgs,
+  shapeName = SHAPE_NAME
+) => {
+  const { scene } = sceneEventArgs;
+  const square = scene.getMeshByName(shapeName) as Mesh;
   while (square.instances.length) {
     square.instances.forEach((inst) => {
-      inst.dispose()
-    })
+      inst.dispose();
+    });
   }
-  square.rotation = Vector3.Zero()
-}
+  square.rotation = Vector3.Zero();
+};
 
-const generateRotation = (config: GenerationConfig): Vector3 =>
-    new Vector3(
-        config.finalRotationX,
-        config.finalRotationY,
-        config.finalRotationZ
-    ).scale(SHAPE_SIZE)
-
+export const generateRotation = (
+  config: Pick<
+    GenerationConfig,
+    "finalRotationZ" | "finalRotationY" | "finalRotationX"
+  >
+): Vector3 =>
+  new Vector3(
+    config.finalRotationX,
+    config.finalRotationY,
+    config.finalRotationZ
+  ).scale(SHAPE_SIZE);
 const generateCoordinates = (config: GenerationConfig): Vector3[] => {
-  const { totalBlocks } = config
-  const allCoords: Vector3[] = [SHAPE_INITIAL_COORD]
+  const { totalBlocks } = config;
 
-  let nBlocksToGenerate = totalBlocks - 1
+  const allCoords: Vector3[] = [SHAPE_INITIAL_COORD];
+  let nBlocksToGenerate = totalBlocks - 1;
   while (nBlocksToGenerate > 0) {
-    const randomDeltaVector = getRandomVector(config)
-    const newCoord = getNewCoord(allCoords, randomDeltaVector)
+    const randomDeltaVector = getRandomVector(config);
+    const newCoord = getNewCoord(allCoords, randomDeltaVector);
     if (newCoord) {
-      allCoords.push(newCoord)
-      nBlocksToGenerate--
+      allCoords.push(newCoord);
+      nBlocksToGenerate--;
     }
   }
-  return allCoords
-}
-
+  return allCoords;
+};
 const getRandomVector = (config: GenerationConfig): Vector3 => {
-  const { maxDeltaForNextBlock, spreadOnX, spreadOnY, spreadOnZ } = config
+  const { maxDeltaForNextBlock, spreadOnX, spreadOnY, spreadOnZ } = config;
   const getValueOfRandomVector = (n: number): Vector3 => {
-    const absN = Math.abs(n)
+    const absN = Math.abs(n);
     switch (true) {
       case absN < spreadOnX:
-        return new Vector3(1, 0, 0).scale(n >= 0 ? 1 : -1)
+        return new Vector3(1, 0, 0).scale(n >= 0 ? 1 : -1);
       case absN < spreadOnX + spreadOnY:
-        return new Vector3(0, 1, 0).scale(n >= 0 ? 1 : -1)
+        return new Vector3(0, 1, 0).scale(n >= 0 ? 1 : -1);
       default:
-        return new Vector3(0, 0, 1).scale(n >= 0 ? 1 : -1)
+        return new Vector3(0, 0, 1).scale(n >= 0 ? 1 : -1);
     }
-  }
-
-  let vectorsForNextPosition: Vector3[] = []
-  const deltaForNextBlock = getRandomInt(maxDeltaForNextBlock) + 1
-  const totalSpread = spreadOnX + spreadOnY + spreadOnZ
+  };
+  let vectorsForNextPosition: Vector3[] = [];
+  const deltaForNextBlock = getRandomInt(maxDeltaForNextBlock) + 1;
+  const totalSpread = spreadOnX + spreadOnY + spreadOnZ;
   while (vectorsForNextPosition.length < deltaForNextBlock) {
-    const randomN = Scalar.RandomRange(-totalSpread, totalSpread)
-    const randomVector = getValueOfRandomVector(randomN)
-    vectorsForNextPosition.push(randomVector)
+    const randomN = Scalar.RandomRange(-totalSpread, totalSpread);
+    const randomVector = getValueOfRandomVector(randomN);
+    vectorsForNextPosition.push(randomVector);
   }
-
   return vectorsForNextPosition
     .reduce(
       (resultingVector, vector) =>
         (resultingVector = resultingVector.add(vector))
     )
-    .scale(SHAPE_SIZE)
-}
-
+    .scale(SHAPE_SIZE);
+};
 const getNewCoord = (
   existingCoords: Vector3[],
   deltaVector: Vector3
 ): Vector3 | null => {
-  const usedCoords: number[] = []
-  let isOverlapping = true
-  let newCoord: Vector3 = Vector3.Zero()
+  const usedCoords: number[] = [];
+  let isOverlapping = true;
+  let newCoord: Vector3 = Vector3.Zero();
   while (isOverlapping && usedCoords.length < existingCoords.length) {
-    const randomExistingCordIdx = getRandomInt(existingCoords.length)
+    const randomExistingCordIdx = getRandomInt(existingCoords.length);
     // TODO: improve not to restart, but to find closest non-used value
     if (usedCoords.includes(randomExistingCordIdx)) {
-      continue
+      continue;
     } else {
-      usedCoords.push(randomExistingCordIdx)
+      usedCoords.push(randomExistingCordIdx);
     }
-
-    newCoord = existingCoords[randomExistingCordIdx].add(deltaVector)
+    newCoord = existingCoords[randomExistingCordIdx].add(deltaVector);
     isOverlapping = existingCoords.some(
       (existingCoord) => existingCoord.toString() === newCoord.toString()
-    )
+    );
   }
-  return !isOverlapping ? newCoord : null
-}
+  return !isOverlapping ? newCoord : null;
+};
