631ad7b8cca799b741a88f73744935f918e632c6
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-02-06 01:56:46 +0200

    Added sample events

--- utils/Events.ts
+++ utils/Events.ts
@@ -1,10 +1,23 @@
-const e = new Event("Task completed");
-document.dispatchEvent(e);
+// const e = new Event("Task completed");
+// document.dispatchEvent(e);
 
 export type CorrectAnswer = "Correct answer";
 export type WrongAnswer = "Wrong answer";
 export type Help = "Help";
 export const HelpE: Help = "Help";
+
+export type SimpleEventMap = {
+  "Correct answer": Event;
+  "Wrong answer": Event;
+  Help: Event;
+};
+
+export type ComplexEventMap = {
+  "Data: correct shape number": CustomEvent<number>;
+  "Data: actual shape number": CustomEvent<number>;
+};
+
+export type EventMap = SimpleEventMap & ComplexEventMap;
 
 export type DataCorrectShapeNumber = "Data: correct shape number";
 export const DataCorrectShapeNumberE: DataCorrectShapeNumber =
@@ -35,4 +48,21 @@ export function createEvent<T extends AppEvent>(
   data?: T extends AppCustomEvent ? CustomEventInit<EventData<T>> : undefined
 ) {
   return new CustomEvent(type, data);
+}
+
+export function createEvent2<T extends keyof SimpleEventMap>(
+  type: T
+): SimpleEventMap[T];
+export function createEvent2<T extends keyof ComplexEventMap>(
+  type: T,
+  data: ComplexEventMap[T]["detail"]
+): ComplexEventMap[T];
+export function createEvent2<T extends keyof SimpleEventMap | ComplexEventMap>(
+  type: T,
+  data?: unknown
+) {
+  if (!data) {
+    return new Event(type as keyof SimpleEventMap);
+  }
+  return new CustomEvent(type as keyof ComplexEventMap, { detail: data });
 }
