736227483b33b13b76b783c56c3e1d711469ee5b
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-04-16 23:29:03 +0300

    Updated schema

--- prisma/migrations/20220416170745_init/migration.sql
+++ prisma/migrations/20220416170745_init/migration.sql
@@ -48,6 +48,49 @@ CREATE TABLE "VerificationToken" (
     "expires" TIMESTAMP(3) NOT NULL
 );
 
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "taskNumber" INTEGER NOT NULL,
+    "referenceShape" JSONB NOT NULL,
+    "targetShape" JSONB NOT NULL,
+    "angles" JSONB,
+    "imageUrl" TEXT NOT NULL,
+    "correctAnswer" INTEGER NOT NULL,
+    "testId" TEXT NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CompletedTest" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "testId" TEXT NOT NULL,
+
+    CONSTRAINT "CompletedTest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CompletedTask" (
+    "id" TEXT NOT NULL,
+    "time" DOUBLE PRECISION NOT NULL,
+    "answer" INTEGER NOT NULL,
+    "correct" BOOLEAN NOT NULL,
+    "testId" TEXT NOT NULL,
+
+    CONSTRAINT "CompletedTask_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
 
@@ -63,8 +106,26 @@ CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token"
 -- CreateIndex
 CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Test_name_key" ON "Test"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Task_id_testId_key" ON "Task"("id", "testId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CompletedTask_id_testId_key" ON "CompletedTask"("id", "testId");
+
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_testId_fkey" FOREIGN KEY ("testId") REFERENCES "Test"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CompletedTest" ADD CONSTRAINT "CompletedTest_testId_fkey" FOREIGN KEY ("testId") REFERENCES "Test"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CompletedTask" ADD CONSTRAINT "CompletedTask_id_fkey" FOREIGN KEY ("id") REFERENCES "CompletedTest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
