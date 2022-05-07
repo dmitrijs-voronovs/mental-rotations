974cbaea87da79bd0d0d36e7197c8999d1dd5d70
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-05-07 16:33:25 +0300

    Added global styles to support pre-generated image layout

---
+++ components/Games/EventManager/SkipHandler.ts
@@ -0,0 +1,25 @@
+import { KeyDownEventHandler } from "@components/Games/EventManager/KeyDownEventHandler";
+import { Timer } from "../../../utils/LaunchTimer";
+import { Mesh } from "@babylonjs/core";
+import { cleanUp } from "../../../utils/GenerateScene";
+import { PrepareScene } from "@components/Games/Test";
+
+export class SkipHandler extends KeyDownEventHandler {
+  constructor(
+    protected timer: Timer,
+    protected prepareScene: PrepareScene,
+    protected meshes: Mesh[]
+  ) {
+    super();
+  }
+
+  handle(key: string): void {
+    this.timer.stopTimer();
+    cleanUp(this.sceneEventArgs, this.meshes);
+    this.prepareScene({ skipMetadataIncrement: true });
+  }
+
+  test(key: string): boolean {
+    return key === "s";
+  }
+}
