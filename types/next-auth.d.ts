5ab52ada089b5e2ac5c52d060499fcaab5b8cd02
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-05-10 00:21:25 +0300

    Add types for next-auth package user and session itnerfaces

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -9,10 +9,12 @@ declare module "next-auth" {
   interface Session {
     user: {
       role: UserRole;
+      id: string;
     } & DefaultSession["user"];
   }
 
   interface User {
     role: UserRole;
+    id: string;
   }
 }
