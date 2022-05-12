2457733608484d501ab85ac688ffeabca6504a3d
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-05-12 14:45:45 +0300

    Add chakra ui theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "@pagestyles/theme";
 
 export default function MyApp({
   Component,
@@ -8,7 +10,9 @@ export default function MyApp({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 }
---
+++ styles/theme.ts
@@ -0,0 +1,5 @@
+import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
+
+export const theme = extendTheme(
+  withDefaultColorScheme({ colorScheme: "purple" })
+);
