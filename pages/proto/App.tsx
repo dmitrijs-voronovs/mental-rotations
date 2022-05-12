74a7a384035a241084aacb07d49289ea72947119
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-05-13 01:28:26 +0300

    Remove doubled chakra provider

--- pages/proto/App.tsx
+++ pages/proto/App.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 
 import dynamic from "next/dynamic";
 
 const Test = dynamic(() => import("@components/Games/Test"), { ssr: false });
 function App() {
   return (
-    <ChakraProvider>
-      <div style={{ flex: 1, display: "flex", position: "relative" }}>
-        <Test />
-      </div>
-    </ChakraProvider>
+    <div style={{ flex: 1, display: "flex", position: "relative" }}>
+      <Test />
+    </div>
   );
 }
 
