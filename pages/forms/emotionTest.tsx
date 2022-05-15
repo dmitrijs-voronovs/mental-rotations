a3f36b26d3e83266485a10233db165650d7b35ff
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-05-15 16:24:39 +0300

    Add data on form completion

--- pages/forms/emotionTest.tsx
+++ pages/forms/emotionTest.tsx
@@ -1,10 +1,19 @@
 import { Center } from "@chakra-ui/react";
 import { EmotionWheel } from "@components/forms/EmotionWheel";
+import axios from "axios";
+import { Prisma } from "@prisma/client";
 
 export default function EmotionTest() {
   return (
     <Center minHeight={"100vh"} p={5}>
-      <EmotionWheel />
+      <EmotionWheel
+        onSubmit={(values) => {
+          axios.post("/api/tests/completeAdditionalTest", {
+            data: values,
+            type: "EMOTION_WHEEL",
+          } as Pick<Prisma.AdditionalTestUncheckedCreateInput, "type" | "data">);
+        }}
+      />
     </Center>
   );
 }
--- pages/forms/phq9.tsx
+++ pages/forms/phq9.tsx
@@ -1,10 +1,19 @@
 import { Center } from "@chakra-ui/react";
 import { PHQ9 } from "@components/forms/PHQ9";
+import axios from "axios";
+import { Prisma } from "@prisma/client";
 
 export default function phq9() {
   return (
     <Center minHeight={"100vh"} p={5}>
-      <PHQ9 />
+      <PHQ9
+        onSubmit={(values) => {
+          axios.post("/api/tests/completeAdditionalTest", {
+            data: values,
+            type: "PHQ9",
+          } as Pick<Prisma.AdditionalTestUncheckedCreateInput, "type" | "data">);
+        }}
+      />
     </Center>
   );
 }
