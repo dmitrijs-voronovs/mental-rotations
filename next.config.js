695a710f97039237dc0f3361f01918523ae6f736
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-05-16 17:52:22 +0300

    Add caching of test images for an hour

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const withTM = require('next-transpile-modules')(["@babylonjs/core", "dat.gui"]);
+const withTM = require("next-transpile-modules")([
+  "@babylonjs/core",
+  "dat.gui",
+]);
 
 module.exports = withTM({
-  reactStrictMode: true
-})
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/tests/(.*).png",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=3600, s-maxage=3600",
+          },
+        ],
+      },
+    ];
+  },
+});
