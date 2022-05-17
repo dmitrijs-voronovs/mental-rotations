7d2d89603a9eca2ad74a238018742f5b470880ec
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2022-05-18 00:30:20 +0300

    Add default values of currently logged user for user information

--- pages/forms/newUser.tsx
+++ pages/forms/newUser.tsx
@@ -1,14 +1,33 @@
 import { UserDetailsForm } from "@components/forms/UserDetailsForm";
 import { Center } from "@chakra-ui/react";
 import axios from "axios";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { getSession } from "next-auth/react";
+import { prisma } from "@lib/prisma";
 
-export default function NewUser() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const data = await getSession(context);
+  return {
+    props: {
+      userDetails: await prisma.userInfo.findUnique({
+        where: {
+          userId: data!.user.id,
+        },
+      }),
+    },
+  };
+};
+
+export default function NewUser({
+  userDetails,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <Center minHeight={"100vh"} p={5}>
       <UserDetailsForm
         onSubmit={(value) => {
           axios.post("/api/users/details", value);
         }}
+        initialValues={userDetails?.info}
       />
     </Center>
   );
